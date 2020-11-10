df31ef4a0d1e0497a6a295adca80b52dcb133574
Kirill Lee <cyril.lee1997@gmail.com>
2020-11-10 22:07:18 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,54 +1,54 @@
-import menuTemplate from '../templates/menu.hbs'
-import menu from '../src/menu.json'
+import menuTemplate from '../templates/menu.hbs';
+import menu from '../src/menu.json';
 
 //tTemplates
 
-const menuRef = createMenuCards(menu)
+const menuRef = createMenuCards(menu);
 
 function createMenuCards(menu) {
-    return menu.map(menuTemplate).join('')
+    return menu.map(menuTemplate).join('');
 }
 
-const ulRef = document.querySelector('ul')
+const ulRef = document.querySelector('ul');
 
-ulRef.insertAdjacentHTML('beforeend', menuRef)
-
+ulRef.insertAdjacentHTML('beforeend', menuRef);
 
 // theme swtcher
 
 const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
 };
 
 const STORAGE_KEY = 'theme';
 
-const switcher = document.querySelector("#theme-switch-toggle")
-const bodyRef = document.querySelector("body")
+const switcher = document.querySelector('#theme-switch-toggle');
+const bodyRef = document.querySelector('body');
 
-switcher.addEventListener('change', clickHandler)
-switcher.addEventListener('change', setLocalStorage)
+switcher.addEventListener('change', clickHandler);
+switcher.addEventListener('change', setLocalStorage);
 document.addEventListener('DOMContentLoaded', getLocalStorageTheme);
 
 function clickHandler() {
-
-  if (switcher.checked === true) {
-      bodyRef.classList.add(Theme.DARK);
-  bodyRef.classList.remove(Theme.LIGHT);
-  } else {
-  bodyRef.classList.add(Theme.LIGHT);
-  bodyRef.classList.remove(Theme.DARK);
-  } 
+    if (switcher.checked === true) {
+        bodyRef.classList.add(Theme.DARK);
+        bodyRef.classList.remove(Theme.LIGHT);
+    } else {
+        bodyRef.classList.add(Theme.LIGHT);
+        bodyRef.classList.remove(Theme.DARK);
+    }
 }
 
 function setLocalStorage() {
-  switcher.checked ? localStorage.setItem(STORAGE_KEY, Theme.DARK) : localStorage.setItem(STORAGE_KEY, Theme.LIGHT)
+    switcher.checked
+        ? localStorage.setItem(STORAGE_KEY, Theme.DARK)
+        : localStorage.setItem(STORAGE_KEY, Theme.LIGHT);
 }
 
 function getLocalStorageTheme() {
-  const localStoregeTheme = localStorage.getItem(STORAGE_KEY)
-  if (localStoregeTheme === Theme.DARK) {
-    bodyRef.classList.add(Theme.DARK)
-    switcher.checked = true
-  }
+    const localStoregeTheme = localStorage.getItem(STORAGE_KEY);
+    if (localStoregeTheme === Theme.DARK) {
+        bodyRef.classList.add(Theme.DARK);
+        switcher.checked = true;
+    }
 }
